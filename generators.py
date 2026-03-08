6bae2b903a77246821a9971950abf26ad6b25886
sadda <lukas.adam.cr@gmail.com>
2026-03-08 11:50:29 +0000

    Refactor

--- generators.py
+++ generators.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy.interpolate import CubicSpline
 from design import Design
 from geometry import rotate
+from typing import Any
 
 
 def get_arc(R: float, start_deg: float, end_deg: float, n_points: int) -> np.ndarray:
@@ -37,7 +38,11 @@ class BarrierGenerator(ABC):
         pass
 
     @abstractmethod
-    def generate_parameters(self) -> None:
+    def random_parameters(self) -> Any:
+        pass
+
+    @abstractmethod
+    def set_parameters(self, params) -> None:
         pass
 
     @abstractmethod
@@ -49,7 +54,8 @@ class BarrierGenerator(ABC):
         return self.__class__.__name__
 
     def random_barriers(self) -> list[np.ndarray]:
-        self.generate_parameters()
+        pars = self.random_parameters()
+        self.set_parameters(pars)
         return self.generate_barriers()
 
     def split_barrier(self, barrier: np.ndarray) -> list[np.ndarray]:
@@ -134,13 +140,20 @@ class FourStupid(BarrierGenerator):
         x_all, y_all = rotate(x_all, y_all, -45)
         return x_all, y_all
 
-    def generate_parameters(self) -> None:
-        # Parameterize the design
-        self.w_mins = self.w_mins_base + 0.5*np.random.random(4)
+    def random_parameters(self):
+        rand1 = 0.5*np.random.random(4)
+        rand2 = 2+3*np.random.random()
+        rand3 = 2+3*np.random.random()
+        rand4 = 2+2*np.random.random()
+        return rand1, rand2, rand3, rand4
+
+    def set_parameters(self, params) -> None:
+        rand1, rand2, rand3, rand4 = params
+        self.w_mins = self.w_mins_base + rand1
         y_min0 = 13+4*np.random.random()
-        y_min1 = y_min0+self.w_mins[0]+2+3*np.random.random()
-        y_min2 = y_min1+self.w_mins[1]+2+3*np.random.random()
-        y_min3 = y_min2+self.w_mins[2]+2+2*np.random.random()
+        y_min1 = y_min0+self.w_mins[0] + rand2
+        y_min2 = y_min1+self.w_mins[1] + rand3
+        y_min3 = y_min2+self.w_mins[2] + rand4
         self.w_mids = self.w_mins
         self.y_mins = np.array([y_min0, y_min1, y_min2, y_min3])
         self.y_mids = self.y_mins + np.array([2, 1.5, 1, 0.5])
@@ -179,9 +192,15 @@ class AbstractHacklGenerator(BarrierGenerator):
     def _bezier_point(self, x: float, y: float, is_inner: bool, order: int) -> tuple[float, float]:
         pass
 
-    def generate_parameters(self) -> None:
-        self.phis_inner = self.phis_inner_min + (self.phis_inner_max-self.phis_inner_min)*np.random.rand(self.n_barriers)
-        self.phis_outer = self.phis_outer_min + (self.phis_outer_max-self.phis_outer_min)*np.random.rand(self.n_barriers)
+    def random_parameters(self):
+        phis_inner = self.phis_inner_min + (self.phis_inner_max-self.phis_inner_min)*np.random.rand(self.n_barriers)
+        phis_outer = self.phis_outer_min + (self.phis_outer_max-self.phis_outer_min)*np.random.rand(self.n_barriers)
+        return phis_inner, phis_outer
+
+    def set_parameters(self, params) -> None:
+        phis_inner, phis_outer = params
+        self.phis_inner = phis_inner
+        self.phis_outer = phis_outer
 
     def generate_barriers(self) -> list[np.ndarray]:
         barriers = []
@@ -232,9 +251,15 @@ class HacklGenerator_OneLambda(AbstractHacklGenerator):
         ub = np.concatenate((self.phis_inner_max, [self.lam_max]))
         return lb, ub
     
-    def generate_parameters(self):
-        self.lam = self.lam_min + (self.lam_max-self.lam_min)*np.random.rand(1)[0]
-        super().generate_parameters()
+    def random_parameters(self):
+        pars = super().random_parameters()
+        lam = self.lam_min + (self.lam_max-self.lam_min)*np.random.rand(1)[0]
+        return *pars, lam
+    
+    def set_parameters(self, params) -> None:
+        phis_inner, phis_outer, lam = params
+        super().set_parameters((phis_inner, phis_outer))
+        self.lam = lam
 
     def _bezier_point(self, x: float, y: float, is_inner: bool, order: int) -> tuple[float, float]:
         return self.lam * x + (1 - self.lam) * y, y
@@ -253,9 +278,15 @@ class HacklGenerator_OneLambdaTheta(HacklGenerator_OneLambda):
         self.theta_max = 20
         super().__init__(design, r_stator_end, **kwargs)
 
-    def generate_parameters(self):
-        self.theta = self.theta_min + (self.theta_max-self.theta_min)*np.random.rand(1)[0]
-        super().generate_parameters()
+    def random_parameters(self):
+        pars = super().random_parameters()
+        theta = self.theta_min + (self.theta_max-self.theta_min)*np.random.rand(1)[0]
+        return *pars, theta
+
+    def set_parameters(self, params) -> None:
+        phis_inner, phis_outer, lam, theta = params
+        super().set_parameters((phis_inner, phis_outer, lam))
+        self.theta = theta
 
     def _bezier_point(self, x: float, y: float, is_inner: bool, order: int) -> tuple[float, float]:
         x, y = super()._bezier_point(x, y, is_inner, order)
@@ -280,10 +311,17 @@ class HacklGenerator_TwoLambdas(AbstractHacklGenerator):
         self.lam_outer_max = 0.45
         super().__init__(design, r_stator_end, **kwargs)
 
-    def generate_parameters(self):
-        self.lam_inner = self.lam_inner_min + (self.lam_inner_max-self.lam_inner_min)*np.random.rand(1)[0]
-        self.lam_outer = self.lam_outer_min + (self.lam_outer_max-self.lam_outer_min)*np.random.rand(1)[0]
-        super().generate_parameters()
+    def random_parameters(self):
+        pars = super().random_parameters()
+        lam_inner = self.lam_inner_min + (self.lam_inner_max-self.lam_inner_min)*np.random.rand(1)[0]
+        lam_outer = self.lam_outer_min + (self.lam_outer_max-self.lam_outer_min)*np.random.rand(1)[0]
+        return *pars, lam_inner, lam_outer
+
+    def set_parameters(self, params) -> None:
+        phis_inner, phis_outer, lam_inner, lam_outer = params
+        super().set_parameters((phis_inner, phis_outer))
+        self.lam_inner = lam_inner
+        self.lam_outer = lam_outer
 
     def _bezier_point(self, x: float, y: float, is_inner: bool, order: int) -> tuple[float, float]:
         lam = self.lam_inner if is_inner else self.lam_outer

eea815a614aa20018952090d1ba27e5f48189ccd
sadda <lukas.adam.cr@gmail.com>
2026-03-17 09:48:49 +0000

    Refactor

---
+++ machine_design/__init__.py
@@ -0,0 +1,3 @@
+from .design import Design
+from .generators import HacklGenerator_OneLambda, HacklGenerator_OneLambdaTheta, HacklGenerator_TwoLambdas, FourStupid, save_params
+from .geometry import analyze_results, plot_barriers, rotate

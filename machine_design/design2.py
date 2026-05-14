2289685f54e536bfd037e770b72e38cf242f6009
sadda <lukas.adam.cr@gmail.com>
2026-05-14 13:11:13 +0000

    Design2 update

--- machine_design/design2.py
+++ machine_design/design2.py
@@ -38,35 +38,3 @@ class Design2(Design):
     def set_derived_params(self):
         pass
 
-    def add_rotor(self) -> None:
-        modeler = self.m2d.modeler
-        assert isinstance(modeler, Modeler2D)
-
-        rotor_id = modeler.create_polyline(
-            points=self.rot_points, segment_type=["Arc", "Line", "Arc"], cover_surface=True, name="Rotor"
-        )
-        self.rotor_id = rotor_id
-        rotor_id.material_name = self.Fe
-        rotor_id.color = (192, 192, 192)  # rgb
-        rotor_id.transparency = 0.0
-
-    def add_rotor_barrier(self, barrier_points, segment_type=None) -> None:
-        modeler = self.m2d.modeler
-        assert isinstance(modeler, Modeler2D)
-
-        # Round it for Ansys
-        barrier_points = np.round(barrier_points, 6)
-
-        # Potentially add the z axis
-        if barrier_points.shape[1] == 2:
-            barrier_points = np.hstack((barrier_points, np.zeros((len(barrier_points), 1))))
-
-        # Convert them into a string format and interpolate
-        points_str = [[str(y) for y in x] for x in barrier_points]
-        barrier_id = modeler.create_polyline(
-            points=points_str, segment_type=segment_type, cover_surface=True, name="Barrier"
-        )
-
-        # Remove the barrier
-        self.rotor_id.subtract(barrier_id)
-        modeler.delete(barrier_id)

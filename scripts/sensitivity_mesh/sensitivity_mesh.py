3fc9a61554db92df012501a672acd03bd0848115
haitao-ZCU <zhuht728@gmail.com>
2026-09-07 08:54:11 +0200

    Use load_config() instead of hardcoding Ansys version and core count

--- scripts/sensitivity_mesh/sensitivity_mesh.py
+++ scripts/sensitivity_mesh/sensitivity_mesh.py
@@ -3,13 +3,15 @@ import os
 import numpy as np
 import pandas as pd
 
+from machine_design.config import load_config
 from machine_design.designs import load_design
 from machine_design.optimization import HacklGenerator_3BrokenLines, HacklGenerator_SixLambdas, analyze_results
 from motors.motor1 import Computation, Geometry
 
-aedt_version = "2025.1"
+config = load_config()
+aedt_version = config["aedt_version"]
+num_cores = config["num_cores"]
 design_name = "Design01"
-num_cores = 4
 r_stator_end = 0.7
 offset = 0.7 / 2
 
